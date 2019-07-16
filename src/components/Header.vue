50a5fd4728a51443365473e7365ef6657597c0b7
zy <madouzaizi@163.com>
2019-07-16 23:04:08 +0800

    home header

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: .86rem
+    background-color: #00bcd4
+    color: #ffffff
+    .header-left
+      width: .64rem
+      /*float: left*/
+      .back-icon
+          /*margin: 0 auto*/
+          text-align center
+    .header-input
+      flex:1
+      margin: .12rem .2rem
+      background :#ffffff
+      border-radius: .1rem
+      color: #cccccc
+      height:.64rem
+      line-height: .64rem
+    .header-right
+      width: 1.24rem
+      /*float: right*/
+      text-align: center
+</style>
